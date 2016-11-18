e48fcb24fa5473d6eeccf03d01248c201515b908
RohanArihant <rohanarihant@users.noreply.github.com>
2016-11-18 21:58:48 +0530

    Add files via upload

---
+++ app/login/login.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+     moduleId: module.id,
+    selector: 'login',
+    templateUrl: 'login.component.html'
+})
+export class LoginComponent { }
