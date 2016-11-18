0afcc0ab1611bd2c5688b94b7bcde1cc7c5975a9
RohanArihant <rohanarihant@users.noreply.github.com>
2016-11-18 22:02:47 +0530

    Add files via upload

---
+++ app/users/users.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+     moduleId: module.id,
+    selector: 'users',
+    templateUrl: 'users.component.html'
+})
+export class UsersComponent { }
