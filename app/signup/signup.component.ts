f0d6f27eb1a34d30592f7a5726dab6c063a16747
RohanArihant <rohanarihant@users.noreply.github.com>
2016-11-18 22:00:40 +0530

    Add files via upload

---
+++ app/signup/signup.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+     moduleId: module.id,
+    selector: 'signup',
+    templateUrl: 'signup.component.html'
+})
+export class SignupComponent { }
